9d5206735cc3879edf0405107908467232466b61
Yasser Elassal <yasser.elassal@AssetDev.com>
2022-08-31 16:11:56 +0200

    fix test class

--- src/test/java/com/example/asset/ApiApplicationTests.java
+++ src/test/java/com/example/asset/ApiApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.asset;
 
+import com.example.asset.main.ApiApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ApiApplication.class)
 class ApiApplicationTests {
 
 	@Test
